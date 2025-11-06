0a01340d17c88fc5ee4f9f75dc8daa9779097b48
Muhammad Farooq <fm6182218@gmail.com>
2025-11-07 00:32:57 +0500

    Done

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -9,13 +9,23 @@ function Color({ onClick }) {
     "purple",
     "pink",
   ];
+  const colorClasses = {
+    blue: "bg-blue-500",
+    red: "bg-red-500",
+    green: "bg-green-500",
+    yellow: "bg-yellow-500",
+    orange: "bg-orange-500",
+    gray: "bg-gray-500",
+    purple: "bg-purple-500",
+    pink: "bg-pink-500",
+  };
   return (
     <div className="flex flex-wrap justify-center items-center gap-3 px-3 rounded-full bg-black py-3">
-      {colors.map((clr) => (
+      {colors.map((clr, indx) => (
         <button
-          key={clr}
+          key={indx}
           onClick={() => onClick(clr)}
-          className={`bg-${clr}-500 px-3 py-2 text-white rounded-2xl`}
+          className={`${colorClasses[clr]} px-3 py-2 text-white rounded-2xl`}
         >
           {clr}
         </button>
